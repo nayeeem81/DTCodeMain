666710ff2f60ff53fb731ac7bc5115f63eb37ed3
nayeeem81 <naimul.prodhan81@hotmail.com>
2026-05-14 23:57:17 +0600

    Pust to remote

--- Main.WebAppCore/Program.cs
+++ Main.WebAppCore/Program.cs
@@ -1,23 +1,23 @@
-using Main.Infrastructure;
-using Microsoft.AspNetCore.Identity;
+//using Main.Infrastructure;
+//using Microsoft.AspNetCore.Identity;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddInfrastructureServices ( builder.Configuration );
+//builder.Services.AddInfrastructureServices ( builder.Configuration );
 
-builder.Services.AddIdentity<IdentityUser,IdentityRole> ( options =>
-{
-    options.SignIn.RequireConfirmedAccount = false;
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequiredLength = 8;
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes ( 5 );
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.User.RequireUniqueEmail = true;
-} );
+//builder.Services.AddIdentity<IdentityUser,IdentityRole> ( options =>
+//{
+//    options.SignIn.RequireConfirmedAccount = false;
+//    options.Password.RequireDigit = true;
+//    options.Password.RequireLowercase = true;
+//    options.Password.RequireUppercase = true;
+//    options.Password.RequireNonAlphanumeric = true;
+//    options.Password.RequiredLength = 8;
+//    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes ( 5 );
+//    options.Lockout.MaxFailedAccessAttempts = 5;
+//    options.User.RequireUniqueEmail = true;
+//} );
 
 
 var app = builder.Build();
